88ba78150b10d3f50ab986d917e82789ef9b266b
megwilliscreative <meg@megwilliscreative.com>
2020-07-31 12:02:34 +1000

    getting popup to show on /test/test-email-popup.html

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,13 +1,13 @@
 // This is where it all goes :)
 
-var openBtn = document.getElementById('openModalBtn');
-var closeBtn = document.getElementById('closeModalBtn');
-var modal = document.getElementById('modal');
+// var openBtn = document.getElementById('openModalBtn');
+// var closeBtn = document.getElementById('closeModalBtn');
+// var modal = document.getElementById('modal');
 
-openBtn.addEventListener('click', function  () {
-	modal.classList.add('open');
-});
+// openBtn.addEventListener('click', function  () {
+// 	modal.classList.add('open');
+// });
 
-closeBtn.addEventListener('click', function () {
-	modal.classList.remove('open');
-});
+// closeBtn.addEventListener('click', function () {
+// 	modal.classList.remove('open');
+// });
