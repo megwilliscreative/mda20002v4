f0fa0172d9ce58f44ba66656b6bf86c856d094a3
megwilliscreative <meg@megwilliscreative.com>
2020-08-03 12:06:26 +1000

    edits to _download_list_item css

--- lib/post_helpers.rb
+++ lib/post_helpers.rb
@@ -6,9 +6,9 @@ module PostHelpers
     !data.website.post.nil? ? data.website.post.sort_by{ |id, post| post.post_date }.reverse : []
   end
 
-  # def stories
-  #   all_posts.select{ |id, post| !post.tags.nil? && post.tags.include?("Story") }
-  # end
+  def stories
+    all_posts.select{ |id, post| !post.tags.nil? && post.tags.include?("Story") }
+  end
 
   def posts_by_tag(tag_name)
     all_posts.select{ |id, post| !post.tags.nil? && post.tags.include?(tag_name) }
