d805aff29869e1cd85024430fd9eebca37e2db5c
megwilliscreative <meg@megwilliscreative.com>
2020-08-07 13:58:14 +1000

    popup styled and laid out. W/ js errors

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,7 +1,9 @@
+// on page ready
 $(document).ready(function ()
-{
+  {
   //Fade in delay for the background overlay (control timing here)
   $("#bkgOverlay").delay(4800).fadeIn(400);
+
   //Fade in delay for the popup (control timing here)
   $("#delayedPopup").delay(5000).fadeIn(400);
   
