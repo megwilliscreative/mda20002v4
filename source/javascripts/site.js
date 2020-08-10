2509f612a7225eff7a311e16fe844252642d5a6f
Tim Lindsay <tim@home>
2020-08-10 20:51:21 +1000

    tweak for remembering user visited

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -15,16 +15,13 @@ $(document).ready(function () {
     //Fade in delay for the background overlay (control timing here)
     $("#bkgOverlay").delay(4800).fadeIn(400);
 
-    window.setTimeout(function(){
-      localStorage.setItem('iwashere',true);
-    },5000);
-
     //Fade in delay for the popup (control timing here)
     $("#delayedPopup").delay(5000).fadeIn(400);
 
     //Hide dialog and background when the user clicks the close button
     $("#btnClose").click(function (e) {
       HideDialog();
+      localStorage.setItem('iwashere',true);
       e.preventDefault();
     });
 
